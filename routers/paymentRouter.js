318e445f6170c645c29fd68506a12641db7c551b
Md. Saiful Islam <80346558+Coder-Saiful@users.noreply.github.com>
2023-05-13 08:40:42 -0600

    Update paymentRouter.js

--- routers/paymentRouter.js
+++ routers/paymentRouter.js
@@ -1,12 +1,21 @@
 const router = require('express').Router();
 const {
     initPayment,
-    ipn
+    ipn,
+    paymentSuccess,
+    paymentFail,
+    paymentCancel
 } = require('../controllers/paymentController');
 const authorize = require('../middlewares/authorize');
 
 router.get('/', authorize, initPayment);
 
 router.post('/ipn', ipn);
+
+router.post('/success', paymentSuccess);
+
+router.post('/fail', paymentFail);
+
+router.post('/cancel', paymentCancel);
 
 module.exports = router;
